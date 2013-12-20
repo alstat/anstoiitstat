3ad49ff8cf681d05a27c4e6bfb0429cb34e485f6
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-12-20 22:44:35 +0800

    Create r.exp.R

---
+++ Johniel-Babiera/r.exp.R
@@ -0,0 +1,7 @@
+johniel <- function(size, rate = 1){
+  r.samples <- rexp(n = size, rate = rate)
+  while(sum(r.samples) >= 460){
+    r.samples <- rexp(n = size, rate = rate)
+  }
+  return(r.samples)
+}
